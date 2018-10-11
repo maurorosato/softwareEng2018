08b8c423e31069e840afade4189ce2b78136bc9b
mauro rosato <mauro10.rosato@gmail.com>
2018-10-11 10:36:05 +0200

    tentativo

--- src/main/java/it/unisalento/se/saw/restapi/StrumentazioneRestController.java
+++ src/main/java/it/unisalento/se/saw/restapi/StrumentazioneRestController.java
@@ -7,7 +7,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +23,6 @@ import it.unisalento.se.saw.domain.Strumentazione;
 import it.unisalento.se.saw.dto.StrumentazioneDto;
 import it.unisalento.se.saw.exceptions.AulaNotFoundException;
 import it.unisalento.se.saw.exceptions.StrumentazioneNotFoundException;
-
 
 @RestController()
 @RequestMapping(value="/strumentazione")
